7f87a6d1a8031c95639261aff648aa5dbd1ec181
K.ShivaNagendra <shivaaman751@gmail.com>
2024-04-23 14:52:30 -0400

    Initial Commit

---
+++ Dice_Simulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice_Simulator
+//
+//  Created by shiva on 4/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
